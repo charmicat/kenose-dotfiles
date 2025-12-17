3fece1fafe3c6328bed49963eece3daf7c8121c6
Luiza Utsch <charmi.cat@gmail.com>
2025-12-17 21:38:57 +0000

    improvements

--- dotfiles/.config/hypr/scripts/launcher.sh
+++ dotfiles/.config/hypr/scripts/launcher.sh
@@ -3,20 +3,25 @@
 # -----------------------------------------------------
 # Load Launcher
 # -----------------------------------------------------
-launcher=$(cat $HOME/.config/ml4w/settings/launcher)
+launcher=$(cat "$HOME"/.config/ml4w/settings/launcher)
 
 # Use Walker
 _launch_walker() {
-    $HOME/.config/walker/launch.sh --height 500
+    "$HOME"/.config/walker/launch.sh --height 500
 }
 
 # Use Rofi
 _launch_rofi() {
-    pkill rofi || rofi -show drun -replace -i  
+    pkill rofi || rofi -show drun -replace -i
+}
+
+# Use Rofi+UWSM
+_launch_rofi_uwsm() {
+    pkill rofi || rofi -show drun -run-command "uwsm app -- {cmd}"
 }
 
 if [ "$launcher" == "walker" ]; then
     _launch_walker
 else
-    _launch_rofi
+    _launch_rofi_uwsm
 fi
